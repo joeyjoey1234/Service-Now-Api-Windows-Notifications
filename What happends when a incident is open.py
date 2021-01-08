0b7842cfecb85165ce0460bea646871a75865c59
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2021-01-07 20:11:14 -0700

    Update What happends when a incident is open.py

--- What happends when a incident is open.py
+++ What happends when a incident is open.py
@@ -1,4 +1,4 @@
 import os
 
-inc = os.system('start https://wwtms.service-now.com/nav_to.do?uri=%2Fincident_list.do%3Fsysparm_query%3Dactive%253Dtrue%255Eassignment_groupISEMPTY%26sysparm_first_row%3D1%26sysparm_view%3D')
+inc = os.system('start https://dev68698.service-now.com/nav_to.do?uri=%2Fincident_list.do%3Fsysparm_query%3Dactive%253Dtrue%255Eassignment_groupISEMPTY%26sysparm_first_row%3D1%26sysparm_view%3D')
 call = os.system('')
